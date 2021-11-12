cc8ca6b9f8d7ffcd89d70bfd8f4612234dc1dfea
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-12 14:13:07 -0500

    first

---
+++ getDinner.py
@@ -0,0 +1,46 @@
+from api_key import key
+
+coordinate = '42.370053, -71.083380'
+keyword = 'restaurant'
+radius = '20'
+api_key = key
+
+import pandas as pd, numpy as np
+import requests
+import json
+import time
+import os
+final_data = []
+
+os.chdir("C:/Users/Jmen3/Desktop/Programs/Python/Dinner Recommender/")
+
+
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+coordinate+'&radius='+str(radius)+'&keyword='+str(keyword)+'&key='+str(api_key)
+
+while True:
+    print(url)
+    respon = requests.get(url)
+    jj = json.loads(respon.text)
+    results = jj['results']
+    for result in results:
+        print("AHHHHHH")
+        name = result['name']
+        place_id = result ['place_id']
+        lat = result['geometry']['location']['lat']
+        lng = result['geometry']['location']['lng']
+        rating = result['rating']
+        types = result['types']
+        vicinity = result['vicinity']
+        data = [name, place_id, lat, lng, rating, types, vicinity]
+        final_data.append(data)
+        time.sleep(5)
+    if 'next_page_token' not in jj:
+        break
+    else:
+        next_page_token = jj['next_page_token']
+        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='+str(api_key)+'&pagetoken='+str(next_page_token)
+
+
+labels = ['Place Name','Place ID', 'Latitude', 'Longitude','Rating', 'Types', 'Vicinity']
+export_dataframe_1_medium = pd.DataFrame.from_records(final_data, columns=labels)
+export_dataframe_1_medium.to_csv('data/restaurants.csv')
