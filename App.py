2a41902b47c43dad7739e5832331b9adf256aaa8
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-22 23:31:14 -0500

    update ui

--- App.py
+++ App.py
@@ -14,53 +14,132 @@ from PyQt5.QtCore import Qt
 
 os.chdir("C:/Users/Jmen3/Desktop/Programs/Python/Dinner Recommender/")
 
-class Window(QWidget):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+# importing libraries
+from PyQt5.QtWidgets import *
+import sys
 
-        grid = QGridLayout()
-        grid.addWidget(self.createSlider(), 1, 0)
-        grid.addWidget(self.createSlider(), 0, 1)
-        self.setLayout(grid)
+inputs = []
 
-        self.setWindowTitle("Dinner Recommender")
-        self.resize(400, 300)
-    def createSlider(self):
-        slider = QGroupBox("Slider Example")
-
-        radio1 = QRadioButton("&Radio horizontal slider")
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setFocusPolicy(Qt.StrongFocus)
-        slider.setTickPosition(QSlider.TicksBothSides)
-        slider.setTickInterval(10)
-        slider.setSingleStep(1)
-
-        radio1.setChecked(True)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(slider)
-        vbox.addStretch(1)
-        slider.setLayout(vbox)
-
-        return slider
+class resultsWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        QMessageBox.question(self, 'Results:', "Placeholder", QMessageBox.Ok)
         
+# creating a class
+# that inherits the QDialog class
+class Window(QDialog):
+
+    # constructor
+    def __init__(self):
+        super(Window, self).__init__()
+
+        # setting window title
+        self.setWindowTitle("Python")
+
+        # setting geometry to the window
+        self.setGeometry(100, 100, 300, 400)
+
+        # creating a group box
+        self.formGroupBox = QGroupBox("Form 1")
+
+        # creating spin box to select age
+        self.distance = QSpinBox()
+
+        # creating combo box to select degree
+        self.category = QComboBox()
+
+        # adding items to the combo box
+        self.category.addItems(["Surprise me!", "Chinese", "Pizza","American","Thai","Indian","Mexican","Cafe","Mediterranean","Diner","Fast Food"])
+
+        # creating a line edit
+        self.advLineEdit = QLineEdit()
+
+        # calling the method that create the form
+        self.createForm()
+
+        # creating a dialog button for ok and cancel
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        # adding action when form is accepted
+        self.buttonBox.accepted.connect(self.getInfo)
+
+        # addding action when form is rejected
+        self.buttonBox.rejected.connect(self.reject)
+
+        # creating a vertical layout
+        mainLayout = QVBoxLayout()
+
+        # adding form group box to the layout
+        mainLayout.addWidget(self.formGroupBox)
+
+        # adding button box to the layout
+        mainLayout.addWidget(self.buttonBox)
+
+        # setting lay out
+        self.setLayout(mainLayout)
+
+    # get info method called when form is accepted
+    def getInfo(self):
+
+        # save the form information
+        adventure = format(self.advLineEdit.text())
+        category = format(self.category.currentText())
+        distance = format(self.distance.text())
+        inputs.append(adventure)
+        inputs.append(category)
+        inputs.append(distance)
+        self.show_results()
+        
+    def show_results(self):
+        self.w = resultsWindow()
+        self.w.show()
+
+    # create form method
+    def createForm(self):
+
+        # creating a form layout
+        layout = QFormLayout()
+
+        # adding rows
+        # for name and adding input text
+        layout.addRow(QLabel("Are you feeling adventurous? (1 = yes, 0 = no):"), self.advLineEdit)
+
+        # for degree and adding combo box
+        layout.addRow(QLabel("Enter a category"), self.category)
+
+        # for age and adding spin box
+        layout.addRow(QLabel("Max distance willing to travel (in miles)"), self.distance)
+
+        # setting layout
+        self.formGroupBox.setLayout(layout)
+
+
+# main method
 if __name__ == '__main__':
+    # create pyqt5 app
     app = QApplication(sys.argv)
-    clock = Window()
-    clock.show()
-    sys.exit(app.exec_())
+
+    # create the instance of our Window
+    window = Window()
+
+    # showing the window
+    window.show()
+    
+    # start the app
+    sys.exit(app.exec())
+
+
 
 app = QApplication(sys.argv)
 
 
 
 ###########################################################
-
-adventure = input("Are you feeling adventurous? (1 = yes, 0 = no): ")
-distance = input("Max distance (in miles): ")
-category = input("Categories?: ")
 
 restaurants = makeData(adventure,distance,category, price)
 restaurants_cat = getCategory(restaurants,'Restaurant', 'Vicinity')
